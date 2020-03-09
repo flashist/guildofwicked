3f885022e3819934f827c732c4822612ce587a1c
Mark Dolbyrev <mark.dolbyrev@skywindgroup.com>
2020-03-09 22:07:09 +0300

    Preloader page: in progress.

--- src/GOWSettings.ts
+++ src/GOWSettings.ts
@@ -1,6 +1,7 @@
 export const GOWSettings = {
     colors: {
         black: 0x272727,
+        grey: 0x767973,
         white: 0xeaeaea
     }
 };
--- src/modules/preloader/views/GOWPreloaderPageView.ts
+++ src/modules/preloader/views/GOWPreloaderPageView.ts
@@ -1,4 +1,5 @@
-import {FContainer, FLabel, getInstance, getText, Graphics, Sprite, Texture} from "fsuite";
+import {NumberTools} from "fcore";
+import {FContainer, FLabel, getInstance, getText, Graphics, Sprite} from "fsuite";
 
 import {TweenLite} from "gsap";
 
@@ -39,11 +40,19 @@ export class GOWPreloaderPageView extends GOWBasePageView {
         this.titleField.x = this.sizeArea.x + Math.floor((this.sizeArea.width - this.titleField.width) / 2);
         this.titleField.y = 510;
 
+        this.progressLabel = new FLabel({
+            fontFamily: "Clarence",
+            size: 48,
+            color: GOWSettings.colors.grey,
+            autosize: true
+        });
+        this.contentCont.addChild(this.progressLabel);
+
         this.progressBg = Sprite.from("loading_bar_transparent.png");
         this.contentCont.addChild(this.progressBg);
         //
         this.progressBg.x = this.sizeArea.x + Math.floor((this.sizeArea.width - this.progressBg.width) / 2);
-        this.progressBg.y = this.titleField.y + this.titleField.height + 100;
+        this.progressBg.y = 650;
 
         this.progressBar = Sprite.from("loading_bar_grey.png");
         this.contentCont.addChild(this.progressBar);
@@ -68,32 +77,6 @@ export class GOWPreloaderPageView extends GOWBasePageView {
         this.progressBar.mask = this.progressBarMask;
 
         this.viewProgress = 0;
-
-        /*this.tapToPlayBtn = new Sprite();
-        this.tapToPlayBtn.texture = Texture.fromFrame("tap_to_play");
-        this.contentCont.addChild(this.tapToPlayBtn);
-        this.tapToPlayBtn.x = this.sizeArea.x + Math.floor((this.sizeArea.width - this.tapToPlayBtn.width) / 2);
-        this.tapToPlayBtn.y = Math.floor(this.titleField.y + this.titleField.height + 100);*/
-
-        /*this.tapToPlayField = new FLabel({
-            fontFamily: "PT Serif",
-            size: 64,
-            color: 0xfff6e8,
-            bold: true,
-            autosize: true,
-            dropShadow: true
-        });
-        this.contentCont.addChild(this.tapToPlayField);
-        // this.tapToPlayField.text = "TAP TO PLAY";
-        this.tapToPlayField.text = getText("tapToPlay");
-        this.tapToPlayField.alpha = 0.75;
-        this.tapToPlayField.x = this.sizeArea.x + Math.floor((this.sizeArea.width - this.tapToPlayField.width) / 2);
-        this.tapToPlayField.y = Math.floor(this.titleField.y + this.titleField.height) + 100;
-
-        this.playBtn = getInstance(PlayButtonControllerView);
-        this.contentCont.addChild(this.playBtn);
-        this.playBtn.x = this.sizeArea.x + Math.floor((this.sizeArea.width - this.playBtn.width) / 2);
-        this.playBtn.y = this.tapToPlayField.y + this.tapToPlayField.height;*/
     }
 
     public get loadingProgress(): number {
@@ -122,10 +105,29 @@ export class GOWPreloaderPageView extends GOWBasePageView {
     set viewProgress(value: number) {
         this._viewProgress = value;
 
-        this.progressBarMask.width = this.progressBar.width * this._viewProgress;
+        this.commitViewData();
 
         if (this.viewProgress >= 1) {
             this.globalDispatcher.dispatchEvent(GOWPreloaderPageViewEvent.PROGRESS_COMPLETE);
         }
+    }
+
+    protected commitViewData(): void {
+        this.progressBarMask.width = this.progressBar.width * this._viewProgress;
+        this.progressLabel.text = getText(
+            "preloaderProgress",
+            {
+                progress: Math.floor(this.viewProgress * 100)
+            }
+        );
+
+        this.arrange();
+    }
+
+    protected arrange(): void {
+        this.progressLabel.x = this.sizeArea.x + Math.floor((this.sizeArea.width - this.progressLabel.width) / 2);
+        this.progressLabel.y = 600;
+
+        super.arrange();
     }
 }
