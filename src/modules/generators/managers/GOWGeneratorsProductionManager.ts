45c1565c7dff91d272ff743500b5cc8540c75120
Mark Dolbyrev <mark.dolbyrev@skywindgroup.com>
2020-03-12 01:10:29 +0300

    Game Page: in progress.

---
+++ src/modules/generators/managers/GOWGeneratorsProductionManager.ts
@@ -0,0 +1,10 @@
+import {BaseManager} from "../../../appframework/base/managers/BaseManager";
+
+export class GOWGeneratorsProductionManager extends BaseManager {
+
+    protected construction(...args): void {
+        super.construction(...args);
+
+
+    }
+}
