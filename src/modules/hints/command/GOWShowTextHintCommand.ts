0f29130bb29e82cf0feb643dd08ff3e05e46eeb8
Mark Dolbyrev <mark.dolbyrev@skywindgroup.com>
2020-03-16 23:29:55 +0300

    Game Page: in progress.

--- src/modules/hints/command/GOWShowTextHintCommand.ts
+++ src/modules/hints/command/GOWShowTextHintCommand.ts
@@ -1,11 +1,18 @@
-import {Align, AutosizeType, FContainer, FLabel, ServiceLocatorObjectsPool, VAlign} from "fsuite";
+import {Align, FContainer, FLabel, getInstance, Point, VAlign} from "fsuite";
 
 import {AnimateHintCommand} from "../../../appframework/hints/commands/AnimateHintCommand";
 import {BaseAppCommand} from "../../../appframework/base/commands/BaseAppCommand";
 import {GOWSettings} from "../../../GOWSettings";
 import {IHintConfig} from "../../../appframework/hints/data/IHintConfig";
+import {HintContainerId} from "../../../appframework/hints/data/HintContainerId";
+import {ContainersManager} from "../../../appframework/containers/managers/ContainersManager";
+import {Facade} from "../../../appframework/facade/Facade";
+import {RendererManager} from "../../../appframework/renderer/managers/RendererManager";
 
 export class GOWShowTextHintCommand extends BaseAppCommand {
+
+    protected containersManager: ContainersManager = getInstance(ContainersManager);
+    protected rendererManager: RendererManager = getInstance(RendererManager);
 
     constructor(protected text: string, protected config?: Partial<IHintConfig>) {
         super();
@@ -33,11 +40,32 @@ export class GOWShowTextHintCommand extends BaseAppCommand {
                 dropShadowBlur: 4
             }
         );
-        hintLabel.text = this.text;
-
         hint.addChild(hintLabel);
+        //
+        hintLabel.text = this.text;
         hintLabel.x = -1 * Math.floor(hintLabel.width / 2);
         hintLabel.y = -1 * Math.floor(hintLabel.height);
+
+        if (!this.config.containerId) {
+            this.config.containerId = HintContainerId;
+        }
+        const tempCont: FContainer = this.containersManager.getContainer(this.config.containerId);
+        const localPos = tempCont.toLocal(this.config.startGlobalPos);
+        const leftGlobalPos = tempCont.toGlobal(new Point(
+            localPos.x - hint.width / 2
+        ));
+        const rightGlobalPos = tempCont.toGlobal(new Point(
+            localPos.x + hint.width / 2
+        ));
+
+        if (leftGlobalPos.x < 0) {
+            this.config.startGlobalPos.x += Math.ceil(-1 * leftGlobalPos.x);
+
+        } else if (rightGlobalPos.x > this.rendererManager.rendererWidth) {
+            const rightDelta: number = rightGlobalPos.x - this.rendererManager.rendererWidth;
+            this.config.startGlobalPos.x = Math.floor(this.config.startGlobalPos.x - rightDelta);
+        }
+
 
         new AnimateHintCommand(hint, this.config)
             .execute()
